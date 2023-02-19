975f133d839595c4fccee64aa07ea12b9fcfb700
Tej19479 <golusingh19479@gmail.com>
2023-02-19 23:55:38 +0530

    graycolor myopen

--- resume/myopenvediooperation.py
+++ resume/myopenvediooperation.py
@@ -10,7 +10,9 @@ print("cap",cap)
 while True:
     ret ,frame =cap.read()
     frame =cv2.resize(frame,(500,700))
+    gray =cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow("frame",frame)
+    cv2.imshow("gray",gray)
     k= cv2.waitKey(25)
     if (k=="q"):
         break
